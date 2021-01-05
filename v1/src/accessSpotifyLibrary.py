a46101bba7b4688a5f85b885828bc0ed991d6cec
gva1178 <gva1178@gmail.com>
2021-01-04 19:05:25 -0500

    added .DS_Store to the .gitignore

--- v1/src/accessSpotifyLibrary.py
+++ v1/src/accessSpotifyLibrary.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -12,11 +13,34 @@ def init():
     return sp
 
 
+def printListOfSongTitles(items):
+    for index, item in enumerate(items):
+        print("Track {index}: {name}, by {artist}".format(index=index, name=item['track']['name'], artist=item['track']['artists'][0]['name']))
+
+
+def createCSVOfLibrary(items):
+    print(items[0])
+    songs = []
+    for item in items:
+        songTitle = item['track']['name']
+        songArtist = item['track']['artists'][0]['name']
+        album = item['track']['album']['name']
+        dateAdded = item['added_at']
+        url = item['track']['external_urls']['spotify']
+        if len(url > 1): url = url[0]
+        uri = item['track']['uri']
+        songs.append([songTitle, songArtist, album, dateAdded, url, uri]) 
+    songsDF = pd.DataFrame(songs, columns=["Song_title", "Primary_artist", "Album", "Date_added", "URL", "URI"])
+    print(list(songsDF["URL"]))
+
+
 def accessSpotifyLibrary():
     #print("To implement accessSpotifyLibrary()")
     sp = init()
     results = sp.current_user_saved_tracks(limit=20)
     items = []
+    items += results['items']
+    '''
     while True:
         results = sp.next(results)
         if results is None:
@@ -25,10 +49,10 @@ def accessSpotifyLibrary():
         #print("Results: {results}".format(results=results))
         items += results['items'] # returns just the values from the key,value pairs in results
         #print("Total: {numTracks}".format(numTracks=items[0]['track']['total']))
-    for index, item in enumerate(items):
-        print("Track {index}: {name}, by {artist}".format(index=index, name=item['track']['name'], artist=item['track']['artists'][0]['name']))
-        #print(item)
-   
+    '''
+    #printListOfSongTitles(items)
+    createCSVOfLibrary(items)
+
     return
 
 
